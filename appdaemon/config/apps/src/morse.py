ac779dacfa276f6f53bc7bc2fd3920cabb93df62
Florian Kempenich <Flori@nKempenich.com>
2019-08-14 17:36:57 +0100

    Step 9e - 'MorseCode' Automation - Test short letter

--- appdaemon/config/apps/src/morse.py
+++ appdaemon/config/apps/src/morse.py
@@ -1,10 +1,59 @@
 import appdaemon.plugins.hass.hassapi as hass
 
+morse = {'A': '.-',
+         'B': '-...',
+         'C': '-.-.',
+         'D': '-..',
+         'E': '.',
+         'F': '..-.',
+         'G': '--.',
+         'H': '....',
+         'I': '..',
+         'J': '.---',
+         'K': '-.-',
+         'L': '.-..',
+         'M': '--',
+         'N': '-.',
+         'O': '---',
+         'P': '.--.',
+         'Q': '--.-',
+         'R': '.-.',
+         'S': '...',
+         'T': '-',
+         'U': '..-',
+         'V': '...-',
+         'W': '.--',
+         'X': '-..-',
+         'Y': '-.--',
+         'Z': '--..'}
+
 
 class MorseCode(hass.Hass):
+    durations = None
+    current = None
+
     def initialize(self):
         self.log('MorseCode was initialized')
         self.listen_state(self.on_new_text, 'input_text.morse')
+        self.durations = dict()
+
+    def _update_config(self):
+        self.durations['short'] = float(self.get_state('input_number.morse_short'))
+        self.durations['long'] = float(self.get_state('input_number.morse_long'))
+        self.durations['interval_symbols'] = float(self.get_state('input_number.morse_interval_symbols'))
+        self.durations['interval_letters'] = float(self.get_state('input_number.morse_interval_letters'))
 
     def on_new_text(self, _entity, _attribute, _old, new_text, _kwargs):
-        pass
+        self._update_config()
+        self.current = 1
+        self.short()
+
+    def short(self):
+        start = self.current
+        end = self.current + self.durations['short']
+        next = end + self.durations['interval_symbols']
+
+        self.run_in(lambda _: self.turn_on('switch.demoswitch'), start)
+        self.run_in(lambda _: self.turn_off('switch.demoswitch'), end)
+
+        self.current = next
--- appdaemon/config/apps/src/test_morse.py
+++ appdaemon/config/apps/src/test_morse.py
@@ -1,11 +1,31 @@
+import pytest
 from appdaemontestframework import automation_fixture
 
-from src.morse import MorseCode
+from src.morse import MorseCode, morse
 
 
 @automation_fixture(MorseCode)
 def morse_code():
     pass
+
+
+@pytest.fixture
+def on_new_text(morse_code: MorseCode):
+    return lambda new_text: morse_code.on_new_text(None, None, None, new_text, None)
+
+
+@pytest.fixture
+def mock_duration(given_that):
+    def do_mock_duration(short, long, between_symbols, between_letters):
+        def to_float_str(number):
+            return str(number) + '.0'
+
+        given_that.state_of('input_number.morse_short').is_set_to(to_float_str(short))
+        given_that.state_of('input_number.morse_long').is_set_to(to_float_str(long))
+        given_that.state_of('input_number.morse_interval_symbols').is_set_to(to_float_str(between_symbols))
+        given_that.state_of('input_number.morse_interval_letters').is_set_to(to_float_str(between_letters))
+
+    return do_mock_duration
 
 
 class TestMorseCode:
@@ -14,4 +34,32 @@ class TestMorseCode:
             .listens_to.state('input_text.morse') \
             .with_callback(morse_code.on_new_text)
 
+    def test_short_letter(self, mock_duration, on_new_text, assert_that, time_travel):
+        # Sanity check
+        assert morse['E'] == '.'
 
+        # Given: Duration for short letter == 2s
+        mock_duration(short=2, long=4, between_symbols=10, between_letters=20)
+
+        # When: Receiving short letter
+        on_new_text('E')
+
+        # Then: Light turned on for short duration
+        #
+        # T=0 Nothing happens
+        # T=1 Light should turn on
+        # T=3 Light should turn off (T=1 + short_duration=2)
+        time_travel.assert_current_time(0)
+        assert_that('switch.demoswitch').was_not.turned_on()
+
+        time_travel.fast_forward(1).seconds()
+        time_travel.assert_current_time(1)
+        assert_that('switch.demoswitch').was.turned_on()
+
+        time_travel.fast_forward(1).seconds()
+        time_travel.assert_current_time(2)
+        assert_that('switch.demoswitch').was_not.turned_off()
+
+        time_travel.fast_forward(1).seconds()
+        time_travel.assert_current_time(3)
+        assert_that('switch.demoswitch').was.turned_off()
